17a148ad31e7e0efb0b1dc7be94a20160e8b93a0
frc <fcaumartin@gmail.com>
2016-03-01 14:49:24 +0100

    initial

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static public string StringToken(string str1, char sep, int nb)
+        {
+            string resultat = "";
+            int compteur = 0;
+
+            foreach(char c in str1)
+            {
+                if (c == sep)
+                {
+                    compteur++;
+                }
+                else
+                {
+                    if (compteur == nb)
+                    {
+                        resultat += c;
+                    }
+                }
+            }
+
+            return resultat;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string attendu = "Bob";
+
+            string str1 = "Bob;Rio;0754205389";
+            string resultat;
+            resultat = Program.StringToken(str1, ';', 0);
+
+            Assert.AreEqual(resultat, attendu);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string attendu = "0754205389";
+
+            string str1 = "Bob;Rio;0754205389";
+            string resultat;
+            resultat = Program.StringToken(str1, ';', 2);
+
+            Assert.AreEqual(resultat, attendu);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            string attendu = "";
+
+            string str1 = "Bob;Rio;0754205389";
+            string resultat;
+            resultat = Program.StringToken(str1, ';', -2);
+
+            Assert.AreEqual(resultat, attendu);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            string attendu = "";
+
+            string str1 = "Bob;Rio;0754205389";
+            string resultat;
+            resultat = Program.StringToken(str1, ';', 36);
+
+            Assert.AreEqual(resultat, attendu);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            string attendu = "Bob;Rio;0754205389";
+
+            string str1 = "Bob;Rio;0754205389";
+            string resultat;
+            resultat = Program.StringToken(str1, 'e', 0);
+
+            Assert.AreEqual(resultat, attendu);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            string attendu = "";
+
+            string str1 = "";
+            string resultat;
+            resultat = Program.StringToken(str1, ' ', 12);
+
+            Assert.AreEqual(resultat, attendu);
+        }
+    }
+}
